b8abf08813fd8d4c23b4011d39178194f46d60da
rjehf <seba19983@o2.pl>
2020-02-14 16:17:45 +0100

    Initial commit

---
+++ src/main/java/pl/kazmierski/naukaintelijj/NaukaIntelijjApplication.java
@@ -0,0 +1,13 @@
+package pl.kazmierski.naukaintelijj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaukaIntelijjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaukaIntelijjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kazmierski/naukaintelijj/NaukaIntelijjApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kazmierski.naukaintelijj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaukaIntelijjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
